b1f6f6e647b4ab3e0aa030968c012c8085107bfb
Steven <stevengf14@gmail.com>
2021-11-30 18:00:10 -0500

    V - Header y Slider

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComponent></HeaderComponent>
+    <Slider></Slider>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import Slider from './components/Slider.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent,
+    Slider
   }
 }
 </script>
 
 <style>
+/*
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +28,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+*/
+
+@import './assets/css/styles.css'
 </style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="mi-componente">
+    <h1>Este es MI COMPONENTE</h1>
+    <h2>{{ subtitulo }}</h2>
+    <hr/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiComponente",
+  data() {
+    return {
+      subtitulo: "Soy el subtítulo de mi componente"
+    }
+  }
+}
+</script>
