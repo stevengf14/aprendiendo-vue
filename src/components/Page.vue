2eeb2844afe087efe910f2dd8733f7ae18fcd05c
Steven <stevengf14@gmail.com>
2022-10-31 23:48:03 -0500

    Hooks

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -2,19 +2,38 @@
   <section id="content">
     <h2 class="subheader">Página de Pruebas</h2>
     <h3>{{ id_from_url }}</h3>
+    <button @click="updateName()">Update Name</button>
+    <h4>{{ name }}</h4>
   </section>
 </template>
 
 <script>
 export default {
   name: "Page",
+  created() {
+    console.log("Component created");
+  },
   mounted() {
     this.id_from_url = this.$route.params.id;
+    console.log("Component mounted");
+  },
+  updated() {
+    console.log("Component updated");
+  },
+  destroyed() {
+    console.log("Component distroyed");
   },
   data() {
     return {
       id_from_url: null,
+      name: "Steven",
     };
+  },
+  methods: {
+    updateName() {
+      this.name = "ANDRES";
+      console.log(this.name);
+    },
   },
 };
 </script>
